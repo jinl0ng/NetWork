e8cf6a1ddfa4b074c6e88c2a6c3f478bbbbf00b8
Leslie <jinl0ng@icloud.com>
2017-11-18 14:50:26 +0800

    server bug fixed

--- udpclient.py
+++ udpclient.py
@@ -2,7 +2,7 @@ from socket import *
 serverName = '39.108.6.163'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input')
+message = input('Input')
 clientSocket.sendto(message, (serverName, serverPort))
 modifiedMessage, serverAddres = clientSocket.recvfrom(2048)
 print (modifiedMessage)
