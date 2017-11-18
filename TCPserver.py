11243166b42305891aebc1cdd2091f3d52424b6b
Leslie <jinl0ng@icloud.com>
2017-11-18 20:19:22 +0800

    recvfrom -> recv

--- TCPserver.py
+++ TCPserver.py
@@ -6,7 +6,7 @@ serverSocket.listen(1)
 print("working\n")
 while 1:
     connectionSocket, addr = serverSocket.accept()
-    message = connectionSocket.recvfrom(1024)
+    message = connectionSocket.recv(1024)
     modifiedMessage = message.decode().upper()
     connectionSocket.send(modifiedMessage.encode('utf-8'))
 
