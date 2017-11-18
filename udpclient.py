20341b06fe1e85cbb9c27168aa82487fa20b368b
Leslie <jinl0ng@icloud.com>
2017-11-18 14:48:47 +0800

    ok

--- udpclient.py
+++ udpclient.py
@@ -2,8 +2,8 @@ from socket import *
 serverName = '39.108.6.163'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input')
+message = raw_input('Input')
 clientSocket.sendto(message, (serverName, serverPort))
 modifiedMessage, serverAddres = clientSocket.recvfrom(2048)
-print modifiedMessage
+print (modifiedMessage)
 clientSocket.close()
